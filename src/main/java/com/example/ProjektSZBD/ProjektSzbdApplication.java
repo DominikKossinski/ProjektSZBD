46ba581ca96cbdf7332b135f71e3ba36d9791e4f
DominikKossinski <37476821+DominikKossinski@users.noreply.github.com>
2018-12-10 18:08:51 +0100

    Projekt Created

---
+++ src/main/java/com/example/ProjektSZBD/ProjektSzbdApplication.java
@@ -0,0 +1,12 @@
+package com.example.ProjektSZBD;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektSzbdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjektSzbdApplication.class, args);
+	}
+}
---
+++ src/test/java/com/example/ProjektSZBD/ProjektSzbdApplicationTests.java
@@ -0,0 +1,16 @@
+package com.example.ProjektSZBD;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ProjektSzbdApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
