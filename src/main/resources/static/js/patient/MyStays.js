46d0217a91dc7f24f4d0e02b42df0d86114075d0
SzymG <szymon.gagorek@gmail.com>
2019-01-21 14:29:29 +0100

    Table od stays

--- src/main/resources/static/js/patient/MyStays.js
+++ src/main/resources/static/js/patient/MyStays.js
@@ -11,11 +11,23 @@ function getMyStays() {
         if (data.resp_status === "ok") {
             var stays = data.stays;
             stays.map(function (stay) {
-                //ToDO ładniejsze wyświetlanie informacji
-                var li = document.createElement("li");
-                li.innerText = "Id: " + stay.id + " termin: " + stay.start_date + " doctor id: " + stay.doctor_id;
-                ul.appendChild(li);
+
+                var end_date = stay.end_date;
+                if(end_date === "null"){
+                    end_date = "-";
+                }
+
+                var row = $('<tr/>')
+                    .append($('<td/>', {text: stay.id || ''}))      //ID pobytu
+                    .append($('<td/>', {text: stay.room_id || ''}))             // nr pokoju
+                    .append($('<td/>', {text: stay.start_date || ''}))        //poczatek
+                    .append($('<td/>', {text: end_date || ''}))      //koniec
+                    .append($('<td/>', {text: stay.doctor_id || ''}));    // id lekarza
+
+                $('table tbody').append(row);
+
             })
+
         } else {
             //TODO ładniejsze wyświetlanie błędu
             alert(data.description);
