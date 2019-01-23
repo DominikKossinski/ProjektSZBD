12d119b4fe153a02f0b35a8f01b8855477667fd8
= <dominikkossinski@gmail.com>
2019-01-23 18:35:09 +0100

    AdminPanel page

--- src/main/java/com/example/ProjektSZBD/Controllers/AdminPanelController.java
+++ src/main/java/com/example/ProjektSZBD/Controllers/AdminPanelController.java
@@ -1,13 +1,20 @@
 package com.example.ProjektSZBD.Controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import static com.example.ProjektSZBD.DataGetter.getUserRoles;
 
 @Controller
 public class AdminPanelController {
 
     @GetMapping("/adminPanel")
-    public String getAdminPanel() {
+    public String getAdminPanel(Model model) {
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        getUserRoles(model, authentication);
         return "AdminPanel";
     }
 }
